a30a621b356926d40c6336b08f074ec88efc2362
João <joao.alexandress12@gmail.com>
2022-04-28 10:17:06 -0300

    alteração do exercício de repetição 4

--- Exercícios/ex4_estrutura_repeticao_for.cpp
+++ Exercícios/ex4_estrutura_repeticao_for.cpp
@@ -6,14 +6,14 @@ float altura, raio, volumeCilindro, maiorVolume;
 int main(){
     printf("Quantos alunos tem na sala? ");
     scanf("%d",&alunos);
-    
+
     for(i=1;i<=alunos;i++){
         printf("Qual a altura do cilindro: ");
         scanf("%f",&altura);
         printf("Qual o raio do cilindro: ");
         scanf("%f",&raio);
         volumeCilindro = 3.141592 * raio * raio * altura;
-        printf("A volume do cilindro do aluno %d e de %.2f\n", i, volumeCilindro);
+        printf("\nA volume do cilindro do aluno %d e de %.2f\n", i, volumeCilindro);
 
         //Verifica se o volume atual é maior que o maior volume já digitado
         if (volumeCilindro > maiorVolume){
